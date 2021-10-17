f5645525817fdb381fb77446a48bea1b4907d4f5
minandev <dhimas.18004@mhs.unesa.ac.id>
2021-10-17 20:41:23 +0700

    pertama commit

---
+++ Assets/KayaRaya.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KayaRaya : MonoBehaviour
+{
+    public GameObject coin;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameObject.FindGameObjectWithTag("coin") == null)
+        {
+            coin.SetActive(true);
+        }
+    }
+    
+    
+}
+
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody2D Rb;
+    public float ms;
+    public float jf;
+    void Start()
+    {
+        Rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       float horiz = Input.GetAxisRaw("Horizontal");//a,d , kiri kanan 
+       Rb.velocity = new Vector2(ms * horiz,Rb.velocity.y);
+       if(Input.GetButtonDown("Jump")) // space
+       {
+           Rb.AddForce(new Vector2(0, jf));
+       }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //statement
+        Destroy(gameObject);
+    }
+}
